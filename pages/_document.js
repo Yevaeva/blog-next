5bcae9326c05bfb54113cce52f7eedc1e3ec1203
Yevaeva <yeva.terteryan@gmail.com>
2022-10-19 11:11:14 +0400

    changes

--- pages/_document.js
+++ pages/_document.js
@@ -3,12 +3,14 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang='en'>
         <Head>
+          <meta property='og:image' content='https://ibb.co/09SBW04' />
+          <meta name='twitter:image' content='https://ibb.co/09SBW04' />
           <body>
             <Main />
             <NextScript />
-            <div id="notifications"></div>
+            <div id='notifications'></div>
           </body>
         </Head>
       </Html>
