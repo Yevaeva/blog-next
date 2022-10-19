765f8da9f5073677b92017392e0dcb42bec9745d
Yevaeva <yeva.terteryan@gmail.com>
2022-10-19 11:06:55 +0400

    added image

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
-        <meta name='viewport' content='width=device-width,initial-scale=1'/>
+        <meta name='viewport' content='width=device-width,initial-scale=1' />
+        <meta property='og:image' content='https://ibb.co/09SBW04' />
+        <meta name='twitter:image' content='https://ibb.co/09SBW04' />
       </Head>
       <Component {...pageProps} />
     </Layout>
