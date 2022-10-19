bca1dd30d7e7b438de3a6ffdcb91ef99ad29df67
Yevaeva <yeva.terteryan@gmail.com>
2022-10-19 11:14:57 +0400

    changes

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,15 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
+          <meta property="og:title" content='TITLE OF YOUR WEBSITE'/>
+<meta property='og:image' content='https://ibb.co/09SBW04' />
+          <meta name='twitter:image' content='https://ibb.co/09SBW04' />
+<meta property="og:description" content='DESCRIPTION OF YOUR SITE'/>
+<meta property="og:url" content='https://blog-next-phi.vercel.app/'/>
+<meta property='og:image:width' content='1200' />
+<meta property='og:image:height' content='627' />
+
+<meta property="og:type" content='website'/>
           <meta property='og:image' content='https://ibb.co/09SBW04' />
           <meta name='twitter:image' content='https://ibb.co/09SBW04' />
           <body>
